b6f3af736b2cbc900b364b561f0865e15fddf3b3
Wallison de Oliveira <wallison_93@hotmail.com>
2016-08-16 03:08:40 -0300

    Fix sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show, :destroy]
-  before_action :require_user, except: [:index, :show]
+  before_action :require_user, except: [:index, :show, :new, :create]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
   def index
