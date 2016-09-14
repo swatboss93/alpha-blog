4914ce56e609514365291daca75c67deb3850c01
Wallison de Oliveira <wallison_93@hotmail.com>
2016-09-14 01:36:59 -0300

    Add categories model and tests

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionController::TestCase
+
+  def setup
+    @category = Category.create(name: "sports")
+  end
+
+  test "should get categories index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get categories new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get categories show" do
+    get(:show, params: {id: @category.id})
+    assert_response :success
+  end
+
+end
